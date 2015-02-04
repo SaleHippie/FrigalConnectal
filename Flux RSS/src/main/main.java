056e6f5cfae64d3e6bfc02aa9d390d613d70e252
joly <matthieu.s.joly@gmail.com>
2015-02-04 09:20:15 +0100

    Refactoring et nettoyage du code

--- Flux RSS/src/main/main.java
+++ Flux RSS/src/main/main.java
@@ -2,7 +2,10 @@ package main;
 
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Enumeration;
+import java.util.Hashtable;
 
+import javax.swing.text.html.HTMLDocument.Iterator;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
@@ -16,44 +19,35 @@ import org.w3c.dom.Node;
 public class main {
 	public static void main( String[] arg ) throws Exception
 	{	
-		System.out.println("Actualités : \n");
-		printRssFromURL("http://www.20min.ch/rss/rss.tmpl?type=channel&get=17&lang=ro");
-		System.out.println("Suisse : \n");
-		printRssFromURL("http://www.20min.ch/rss/rss.tmpl?type=rubrik&get=313&lang=ro");
-		System.out.println("Monde : \n");
-		printRssFromURL("http://www.20min.ch/rss/rss.tmpl?type=rubrik&get=312&lang=ro");
-		System.out.println("Economie : \n");
-		printRssFromURL("http://www.20min.ch/rss/rss.tmpl?type=rubrik&get=316&lang=ro");
-		System.out.println("Faits divers : \n");
-		printRssFromURL("http://www.20min.ch/rss/rss.tmpl?type=rubrik&get=318&lang=ro");
-		System.out.println("Vaud : \n");
-		printRssFromURL("http://www.20min.ch/rss/rss.tmpl?type=rubrik&get=315&lang=ro");
-		System.out.println("Geneve : \n");
-		printRssFromURL("http://www.20min.ch/rss/rss.tmpl?type=rubrik&get=314&lang=ro");
-		System.out.println("Romandie : \n");
-		printRssFromURL("http://www.20min.ch/rss/rss.tmpl?type=rubrik&get=784&lang=ro");
-		System.out.println("People : \n");
-		printRssFromURL("http://www.20min.ch/rss/rss.tmpl?type=channel&get=22&lang=ro");
-		System.out.println("Sport : \n");
-		printRssFromURL("http://www.20min.ch/rss/rss.tmpl?type=channel&get=23&lang=ro");
-		System.out.println("Hi-tech : \n");
-		printRssFromURL("http://www.20min.ch/rss/rss.tmpl?type=channel&get=20&lang=ro");
-		System.out.println("Sortir : \n");
-		printRssFromURL("http://www.20min.ch/rss/rss.tmpl?type=rubrik&get=526&lang=ro");
+		Hashtable<String, String> link = new Hashtable<String, String>();
 		
+		link.put("Actualités : \n", "http://www.20min.ch/rss/rss.tmpl?type=channel&get=17&lang=ro");
+		link.put("Suisse : \n", "http://www.20min.ch/rss/rss.tmpl?type=rubrik&get=313&lang=ro");
+		link.put("Monde : \n", "http://www.20min.ch/rss/rss.tmpl?type=rubrik&get=312&lang=ro");
+		link.put("Economie : \n", "http://www.20min.ch/rss/rss.tmpl?type=rubrik&get=316&lang=ro");
+		link.put("Faits divers : \n", "http://www.20min.ch/rss/rss.tmpl?type=rubrik&get=318&lang=ro");
+		link.put("Vaud : \n", "http://www.20min.ch/rss/rss.tmpl?type=rubrik&get=315&lang=ro");
+		link.put("Geneve : \n", "http://www.20min.ch/rss/rss.tmpl?type=rubrik&get=314&lang=ro");
+		link.put("Romandie : \n", "http://www.20min.ch/rss/rss.tmpl?type=rubrik&get=784&lang=ro");
+		link.put("People : \n", "http://www.20min.ch/rss/rss.tmpl?type=channel&get=22&lang=ro");
+		link.put("Sport : \n", "http://www.20min.ch/rss/rss.tmpl?type=channel&get=23&lang=ro");
+		link.put("Hi-tech : \n", "http://www.20min.ch/rss/rss.tmpl?type=channel&get=20&lang=ro");
+		link.put("Sortir : \n", "http://www.20min.ch/rss/rss.tmpl?type=rubrik&get=526&lang=ro");
 
-		//Pour transformer en XML, ça permet de voir si le document est bien construit
-		//TransformerFactory tf = TransformerFactory.newInstance();
-		//Transformer t = tf.newTransformer();
-		//t.transform(new DOMSource(document),new StreamResult(new File("toto.xml")));
-
-
+		Enumeration e = link.keys();
+		while( e.hasMoreElements() ){
+			String key = (String) e.nextElement();
+			System.out.println( key );
+			printRssFromURL( link.get(key) );
+		}
+		
+		
 	}
 
 	public static void printRssFromURL( String urlGet ) throws Exception{
 
-		//File fichier = new File("Toto.xml");
-		URL url = new URL( urlGet ); //("http://fobec.com/CMS/fobec.xml");
+
+		URL url = new URL( urlGet );
 		URLConnection conn = url.openConnection();
 
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
@@ -90,9 +84,6 @@ public class main {
 				fils = fils.getFirstChild();
 
 				while( fils != null ){
-					/*if( fils.getNodeName() == "title" ){
-						ret += fils.getTextContent() + "\n";
-					}*/
 
 					if( fils.hasChildNodes())
 					{
@@ -113,7 +104,6 @@ public class main {
 
 					fils = fils.getNextSibling();
 				}
-
 			}
 		}
 		return ret;
