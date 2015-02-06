1c4bfa5a953d221dde96f40aa917ff12476e7882
joly <matthieu.s.joly@gmail.com>
2015-02-06 15:33:05 +0100

    Mise en place du premier graphique

--- FrigoConnecte/src/Main.java
+++ FrigoConnecte/src/Main.java
@@ -1,13 +1,16 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.URL;
 import java.net.URLConnection;
+import java.text.SimpleDateFormat;
 import java.util.Enumeration;
 import java.util.Hashtable;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -15,16 +18,28 @@ import javax.swing.Timer;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.DateAxis;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.time.Month;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.ui.RectangleInsets;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
 @SuppressWarnings("serial")
 public class Main extends JPanel{
-	//private static JPanel panelWarning = new JPanel();
+	private ChartPanel panelStat;
 	private JButton btnStat = new JButton("Données/Statistiques");
 	private JButton btnParam = new JButton("Paramètres");
-	private JButton moduleMeteo = new JButton();
-
+	private JButton moduleMeteo = new JButton("Espace météo !!!");
+	
+	private JComboBox<String> lstStat = new JComboBox<String>();
+	
 	private static JPanel panelInfo = new JPanel();
 	private JLabel alerte = new JLabel();
 	private JLabel info = new JLabel();
@@ -64,19 +79,41 @@ public class Main extends JPanel{
 
 	Main() throws Exception{
 		infoRSS = getRSSString();
-
 		panelInfo.setPreferredSize(new Dimension( 512,400 ) );
-		//mX = panelInfo.getPreferredSize().width;
-
+		//mX = panelInfo.getPreferredSize().width
 		panelInfo.add(alerte);
+		
 		//implémentation des boutons
 		btnStat.setPreferredSize(new Dimension( 200, 200 ));
 		btnParam.setPreferredSize(new Dimension( 200, 200 ));
 		moduleMeteo.setPreferredSize(new Dimension(407, 100));
+		
+		btnStat.addActionListener( new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO désinstencié panelInfo
+				
+				JFreeChart chart = createChart(createDataset());
+				//padding.trimHeight(350.0);
+				chart.setPadding(new RectangleInsets(60, 100, 100, 100));
+				panelStat = new ChartPanel(chart);
+				panelInfo.setVisible(false);
+				panelStat.add( new JLabel("Stat : "));
+				lstStat.addItem("Donnée en temps réel");
+				lstStat.addItem("Graphique température");
+				panelStat.add(lstStat);
+				
+				panelStat.setVisible(true);
+				add(panelStat);
+			}
+		});
+		
 		btnStat.setVisible(true);
 		btnParam.setVisible(true);
 		info.setText(infoRSS);
-		// interface coponnent
+		
+		// interface componnent
 		panelInfo.add(btnStat);
 		panelInfo.add(btnParam);
 		panelInfo.add(moduleMeteo);
@@ -174,7 +211,7 @@ public class Main extends JPanel{
 						while( petitFils != null){
 
 							if( petitFils.getNodeName() == "title" ){
-								ret += petitFils.getTextContent() + ". ";	
+								ret += petitFils.getTextContent() + ". ";
 							}
 							//if(petitFils.getNodeName() == "link" ){
 							//	ret += petitFils.getTextContent() + " ";	
@@ -189,4 +226,104 @@ public class Main extends JPanel{
 		}
 		return ret;
 	}
+	
+	/**
+     * Creates a dataset, consisting of two series of monthly data.
+     *
+     * @return The dataset.
+     */
+    private static XYDataset createDataset() {
+
+        TimeSeries s1 = new TimeSeries("Humidité");
+        s1.add(new Month(2, 2001), 181.8);
+        s1.add(new Month(3, 2001), 167.3);
+        s1.add(new Month(4, 2001), 153.8);
+        s1.add(new Month(5, 2001), 167.6);
+        s1.add(new Month(6, 2001), 158.8);
+        s1.add(new Month(7, 2001), 148.3);
+        s1.add(new Month(8, 2001), 153.9);
+        s1.add(new Month(9, 2001), 142.7);
+        s1.add(new Month(10, 2001), 123.2);
+        s1.add(new Month(11, 2001), 131.8);
+        s1.add(new Month(12, 2001), 139.6);
+        s1.add(new Month(1, 2002), 142.9);
+        s1.add(new Month(2, 2002), 138.7);
+        s1.add(new Month(3, 2002), 137.3);
+        s1.add(new Month(4, 2002), 143.9);
+        s1.add(new Month(5, 2002), 139.8);
+        s1.add(new Month(6, 2002), 137.0);
+        s1.add(new Month(7, 2002), 132.8);
+
+        TimeSeries s2 = new TimeSeries("Temperature");
+        s2.add(new Month(2, 2001), 129.6);
+        s2.add(new Month(3, 2001), 123.2);
+        s2.add(new Month(4, 2001), 117.2);
+        s2.add(new Month(5, 2001), 124.1);
+        s2.add(new Month(6, 2001), 122.6);
+        s2.add(new Month(7, 2001), 119.2);
+        s2.add(new Month(8, 2001), 116.5);
+        s2.add(new Month(9, 2001), 112.7);
+        s2.add(new Month(10, 2001), 101.5);
+        s2.add(new Month(11, 2001), 106.1);
+        s2.add(new Month(12, 2001), 110.3);
+        s2.add(new Month(1, 2002), 111.7);
+        s2.add(new Month(2, 2002), 111.0);
+        s2.add(new Month(3, 2002), 109.6);
+        s2.add(new Month(4, 2002), 113.2);
+        s2.add(new Month(5, 2002), 111.6);
+        s2.add(new Month(6, 2002), 108.8);
+        s2.add(new Month(7, 2002), 101.6);
+
+        // ******************************************************************
+        //  More than 150 demo applications are included with the JFreeChart
+        //  Developer Guide...for more information, see:
+        //
+        //  >   http://www.object-refinery.com/jfreechart/guide.html
+        //
+        // ******************************************************************
+
+        TimeSeriesCollection dataset = new TimeSeriesCollection();
+        dataset.addSeries(s1);
+        dataset.addSeries(s2);
+
+        return dataset;
+
+    }
+    
+    /**
+     * Creates a chart.
+     *
+     * @param dataset  a dataset.
+     *
+     * @return A chart.
+     */
+    private static JFreeChart createChart(XYDataset dataset) {
+
+        JFreeChart chart = ChartFactory.createTimeSeriesChart(
+            "Stat en temps réel",  // title
+            "Date",             // x-axis label
+            "Temperture",   // y-axis label
+            dataset,            // data
+            true,               // create legend?
+            true,               // generate tooltips?
+            false               // generate URLs?
+        );
+
+        chart.setBackgroundPaint(Color.white);
+
+        XYPlot plot = (XYPlot) chart.getPlot();
+        plot.setBackgroundPaint(Color.white);
+        plot.setDomainGridlinePaint(Color.lightGray);
+        plot.setRangeGridlinePaint(Color.lightGray);
+        plot.setAxisOffset(new RectangleInsets(5.0, 5.0, 5.0, 5.0));
+        plot.setDomainCrosshairVisible(true);
+        plot.setRangeCrosshairVisible(true);
+
+
+        DateAxis axis = (DateAxis) plot.getDomainAxis();
+        axis.setDateFormatOverride(new SimpleDateFormat("MMM-yyyy"));
+
+        return chart;
+
+    }
 }
