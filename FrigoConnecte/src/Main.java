274e470ed01d749a33664735667eeb282fe95b5a
joly <matthieu.s.joly@gmail.com>
2015-02-16 08:42:48 +0100

    Implémentation de la météo

--- FrigoConnecte/src/Main.java
+++ FrigoConnecte/src/Main.java
@@ -31,15 +31,19 @@ import org.jfree.ui.RectangleInsets;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
+import com.teknikindustries.yahooweather.WeatherDisplay;
+import com.teknikindustries.yahooweather.WeatherDoc;
+
+
 @SuppressWarnings("serial")
 public class Main extends JPanel{
 	private ChartPanel panelStat;
 	private JButton btnStat = new JButton("Données/Statistiques");
 	private JButton btnParam = new JButton("Paramètres");
 	private JButton moduleMeteo = new JButton("Espace météo !!!");
-	
+
 	private JComboBox<String> lstStat = new JComboBox<String>();
-	
+
 	private static JPanel panelInfo = new JPanel();
 	private JLabel alerte = new JLabel();
 	private JLabel info = new JLabel();
@@ -82,18 +86,18 @@ public class Main extends JPanel{
 		panelInfo.setPreferredSize(new Dimension( 512,400 ) );
 		//mX = panelInfo.getPreferredSize().width
 		panelInfo.add(alerte);
-		
+
 		//implémentation des boutons
 		btnStat.setPreferredSize(new Dimension( 200, 200 ));
 		btnParam.setPreferredSize(new Dimension( 200, 200 ));
 		moduleMeteo.setPreferredSize(new Dimension(407, 100));
-		
+
 		btnStat.addActionListener( new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO désinstencié panelInfo
-				
+
 				JFreeChart chart = createChart(createDataset());
 				//padding.trimHeight(350.0);
 				chart.setPadding(new RectangleInsets(60, 100, 100, 100));
@@ -103,20 +107,21 @@ public class Main extends JPanel{
 				lstStat.addItem("Donnée en temps réel");
 				lstStat.addItem("Graphique température");
 				panelStat.add(lstStat);
-				
+
 				panelStat.setVisible(true);
 				add(panelStat);
 			}
 		});
-		
+
 		btnStat.setVisible(true);
 		btnParam.setVisible(true);
 		info.setText(infoRSS);
-		
+
 		// interface componnent
 		panelInfo.add(btnStat);
 		panelInfo.add(btnParam);
 		panelInfo.add(moduleMeteo);
+		weather();
 		panelInfo.add(info);
 		ActionListener taskPerformer = new ActionListener() {
 			public void actionPerformed(ActionEvent evt) {
@@ -226,104 +231,158 @@ public class Main extends JPanel{
 		}
 		return ret;
 	}
-	
+
 	/**
-     * Creates a dataset, consisting of two series of monthly data.
-     *
-     * @return The dataset.
-     */
-    private static XYDataset createDataset() {
+	 * Creates a dataset, consisting of two series of monthly data.
+	 *
+	 * @return The dataset.
+	 */
+	private static XYDataset createDataset() {
 
-        TimeSeries s1 = new TimeSeries("Humidité");
-        s1.add(new Month(2, 2001), 181.8);
-        s1.add(new Month(3, 2001), 167.3);
-        s1.add(new Month(4, 2001), 153.8);
-        s1.add(new Month(5, 2001), 167.6);
-        s1.add(new Month(6, 2001), 158.8);
-        s1.add(new Month(7, 2001), 148.3);
-        s1.add(new Month(8, 2001), 153.9);
-        s1.add(new Month(9, 2001), 142.7);
-        s1.add(new Month(10, 2001), 123.2);
-        s1.add(new Month(11, 2001), 131.8);
-        s1.add(new Month(12, 2001), 139.6);
-        s1.add(new Month(1, 2002), 142.9);
-        s1.add(new Month(2, 2002), 138.7);
-        s1.add(new Month(3, 2002), 137.3);
-        s1.add(new Month(4, 2002), 143.9);
-        s1.add(new Month(5, 2002), 139.8);
-        s1.add(new Month(6, 2002), 137.0);
-        s1.add(new Month(7, 2002), 132.8);
+		TimeSeries s1 = new TimeSeries("Humidité");
+		s1.add(new Month(2, 2001), 181.8);
+		s1.add(new Month(3, 2001), 167.3);
+		s1.add(new Month(4, 2001), 153.8);
+		s1.add(new Month(5, 2001), 167.6);
+		s1.add(new Month(6, 2001), 158.8);
+		s1.add(new Month(7, 2001), 148.3);
+		s1.add(new Month(8, 2001), 153.9);
+		s1.add(new Month(9, 2001), 142.7);
+		s1.add(new Month(10, 2001), 123.2);
+		s1.add(new Month(11, 2001), 131.8);
+		s1.add(new Month(12, 2001), 139.6);
+		s1.add(new Month(1, 2002), 142.9);
+		s1.add(new Month(2, 2002), 138.7);
+		s1.add(new Month(3, 2002), 137.3);
+		s1.add(new Month(4, 2002), 143.9);
+		s1.add(new Month(5, 2002), 139.8);
+		s1.add(new Month(6, 2002), 137.0);
+		s1.add(new Month(7, 2002), 132.8);
 
-        TimeSeries s2 = new TimeSeries("Temperature");
-        s2.add(new Month(2, 2001), 129.6);
-        s2.add(new Month(3, 2001), 123.2);
-        s2.add(new Month(4, 2001), 117.2);
-        s2.add(new Month(5, 2001), 124.1);
-        s2.add(new Month(6, 2001), 122.6);
-        s2.add(new Month(7, 2001), 119.2);
-        s2.add(new Month(8, 2001), 116.5);
-        s2.add(new Month(9, 2001), 112.7);
-        s2.add(new Month(10, 2001), 101.5);
-        s2.add(new Month(11, 2001), 106.1);
-        s2.add(new Month(12, 2001), 110.3);
-        s2.add(new Month(1, 2002), 111.7);
-        s2.add(new Month(2, 2002), 111.0);
-        s2.add(new Month(3, 2002), 109.6);
-        s2.add(new Month(4, 2002), 113.2);
-        s2.add(new Month(5, 2002), 111.6);
-        s2.add(new Month(6, 2002), 108.8);
-        s2.add(new Month(7, 2002), 101.6);
+		TimeSeries s2 = new TimeSeries("Temperature");
+		s2.add(new Month(2, 2001), 129.6);
+		s2.add(new Month(3, 2001), 123.2);
+		s2.add(new Month(4, 2001), 117.2);
+		s2.add(new Month(5, 2001), 124.1);
+		s2.add(new Month(6, 2001), 122.6);
+		s2.add(new Month(7, 2001), 119.2);
+		s2.add(new Month(8, 2001), 116.5);
+		s2.add(new Month(9, 2001), 112.7);
+		s2.add(new Month(10, 2001), 101.5);
+		s2.add(new Month(11, 2001), 106.1);
+		s2.add(new Month(12, 2001), 110.3);
+		s2.add(new Month(1, 2002), 111.7);
+		s2.add(new Month(2, 2002), 111.0);
+		s2.add(new Month(3, 2002), 109.6);
+		s2.add(new Month(4, 2002), 113.2);
+		s2.add(new Month(5, 2002), 111.6);
+		s2.add(new Month(6, 2002), 108.8);
+		s2.add(new Month(7, 2002), 101.6);
 
-        // ******************************************************************
-        //  More than 150 demo applications are included with the JFreeChart
-        //  Developer Guide...for more information, see:
-        //
-        //  >   http://www.object-refinery.com/jfreechart/guide.html
-        //
-        // ******************************************************************
+		// ******************************************************************
+		//  More than 150 demo applications are included with the JFreeChart
+		//  Developer Guide...for more information, see:
+		//
+		//  >   http://www.object-refinery.com/jfreechart/guide.html
+		//
+		// ******************************************************************
 
-        TimeSeriesCollection dataset = new TimeSeriesCollection();
-        dataset.addSeries(s1);
-        dataset.addSeries(s2);
+		TimeSeriesCollection dataset = new TimeSeriesCollection();
+		dataset.addSeries(s1);
+		dataset.addSeries(s2);
 
-        return dataset;
+		return dataset;
 
-    }
-    
-    /**
-     * Creates a chart.
-     *
-     * @param dataset  a dataset.
-     *
-     * @return A chart.
-     */
-    private static JFreeChart createChart(XYDataset dataset) {
+	}
 
-        JFreeChart chart = ChartFactory.createTimeSeriesChart(
-            "Stat en temps réel",  // title
-            "Date",             // x-axis label
-            "Temperture",   // y-axis label
-            dataset,            // data
-            true,               // create legend?
-            true,               // generate tooltips?
-            false               // generate URLs?
-        );
+	/**
+	 * Creates a chart.
+	 *
+	 * @param dataset  a dataset.
+	 *
+	 * @return A chart.
+	 */
+	private static JFreeChart createChart(XYDataset dataset) {
 
-        chart.setBackgroundPaint(Color.white);
+		JFreeChart chart = ChartFactory.createTimeSeriesChart(
+				"Stat en temps réel",  // title
+				"Date",             // x-axis label
+				"Temperture",   // y-axis label
+				dataset,            // data
+				true,               // create legend?
+				true,               // generate tooltips?
+				false               // generate URLs?
+				);
 
-        XYPlot plot = (XYPlot) chart.getPlot();
-        plot.setBackgroundPaint(Color.white);
-        plot.setDomainGridlinePaint(Color.lightGray);
-        plot.setRangeGridlinePaint(Color.lightGray);
-        plot.setAxisOffset(new RectangleInsets(5.0, 5.0, 5.0, 5.0));
-        plot.setDomainCrosshairVisible(true);
-        plot.setRangeCrosshairVisible(true);
+		chart.setBackgroundPaint(Color.white);
+
+		XYPlot plot = (XYPlot) chart.getPlot();
+		plot.setBackgroundPaint(Color.white);
+		plot.setDomainGridlinePaint(Color.lightGray);
+		plot.setRangeGridlinePaint(Color.lightGray);
+		plot.setAxisOffset(new RectangleInsets(5.0, 5.0, 5.0, 5.0));
+		plot.setDomainCrosshairVisible(true);
+		plot.setRangeCrosshairVisible(true);
 
 
-        DateAxis axis = (DateAxis) plot.getDomainAxis();
-        axis.setDateFormatOverride(new SimpleDateFormat("MMM-yyyy"));
+		DateAxis axis = (DateAxis) plot.getDomainAxis();
+		axis.setDateFormatOverride(new SimpleDateFormat("MMM-yyyy"));
 
-        return chart;
+		return chart;
 
-    }
+	}
+
+	private void weather(){
+		// 783058 = code de Lausanne / c = celsius
+		// Instancie la classe WeatherDoc se qui initialise les valeurs
+		//Supress Warnigs cause the librery is build like that
+		@SuppressWarnings("unused")
+		WeatherDoc doc = new WeatherDoc("783058", "c");
+		//on créer maintenant un objet dérivé de WeatherDoc pour récupérer les données
+		WeatherDisplay disp = new WeatherDisplay();
+
+		//Affichages dans la consoles des valeurs qui peuvent être utiles
+		System.out.println( "Temp : " + disp.getTemperature() + "°" + disp.getTemperatureUnit() );
+		System.out.println("Humidity : " + disp.getHumidity() + "%");
+		System.out.println(disp.getCity() + " " + disp.getCondition());
+		System.out.println("lever : " + disp.getSunrise());
+		System.out.println("coucher : " + disp.getSunset());
+		String direction = disp.getWindDirection();
+		System.out.println( disp.getWindSpeed() + "m/s" + "|" + (Double.parseDouble(disp.getWindSpeed())*3.6) + "Km/h " + getDirection( Integer.parseInt(direction)  ) + direction );
+		System.out.println("pression : " + disp.getPressure() + disp.getPressureUnit());
+		System.out.println(disp.getCountry() );
+	}
+
+	private String getDirection(int azimuth){
+
+		//parse the azimuth of compass for the common Users
+		if (azimuth <= 22) {
+			return "N";
+		}
+		else if( azimuth <= 67){
+			return "NE";
+		}
+		else if( azimuth <= 112){
+			return "E";
+		}
+		else if( azimuth <= 157){
+			return "SE";
+		}
+		else if( azimuth <= 202){
+			return "S";
+		}
+		else if( azimuth <= 247){
+			return "SW";
+		}
+		else if( azimuth <= 292){
+			return "W";
+		}
+		else if( azimuth <= 337){
+			return "NW";
+		}
+		else{ //case between 338 and 360
+			return "N";
+		}
+
+	}
 }
